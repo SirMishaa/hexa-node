e2e800e974f9951b0ba6e34b0b2789e5fcc0c1ce
SirMishaa <mishaa.pro@proton.me>
2022-12-27 17:33:32 +0100

    :sparkles: create base typescript contract for domain

--- src/infrastructure/controller/HomeController.ts
+++ src/infrastructure/controller/HomeController.ts
@@ -2,6 +2,6 @@ import { FastifyReply, FastifyRequest } from 'fastify'
 
 export class HomeController {
 	handle(request: FastifyRequest, response: FastifyReply) {
-		response.send('Hello World!')
+		response.send({ message: 'Hello World!' })
 	}
 }
---
+++ src/spi.ts
@@ -0,0 +1,38 @@
+type Uuid = string
+
+export interface BaseEntity {
+	id: Uuid
+	createdAt: Date
+	updatedAt: Date
+	removedAt: Date | null
+}
+
+export interface Item extends BaseEntity {
+	name: string
+	description: string
+	price: number
+	availableQuantity: number
+	seller: Seller
+}
+
+export interface Seller {
+	name: string
+	description: string
+	logoPictureUrl: string
+	contactEmail: string
+	soldItems: Array<Item>
+}
+
+export interface CrudOperation<Type extends BaseEntity> {
+	findById(id: Uuid): Promise<Type>
+	findAll(): Promise<Array<Type>>
+	create(entity: Type): Promise<Type>
+	update(entity: Type): Promise<Type>
+	softRemove(id: Uuid): Promise<Type>
+	hardRemove(id: Uuid): Promise<Type>
+}
+
+export interface ItemRepository extends CrudOperation<Item> {
+	findBySellerId(sellerId: Uuid): Promise<Array<Item>>
+	findByName(name: string): Promise<Array<Item>>
+}
